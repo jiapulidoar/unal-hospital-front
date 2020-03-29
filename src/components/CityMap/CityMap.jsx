813ea93b897b018fd340318939d0078ff85f70bc
José Nieto <jdnv.123@gmail.com>
2020-03-29 15:43:08 -0500

    Initialize Map component

--- src/components/CityMap/CityMap.jsx
+++ src/components/CityMap/CityMap.jsx
@@ -1,11 +1,19 @@
 import React from 'react'
+import mapboxgl from 'mapbox-gl'
 
 // import './CityMap.scss'
 
-export default function CityMap() {
-    return (
-        <div id="map-container">
-            
-        </div>
-    )
+export default class CityMap extends React.Component {
+    componentDidMount() {
+        const { REACT_APP_MAPBOX_ACCESS_TOKEN } = process.env
+        console.log(REACT_APP_MAPBOX_ACCESS_TOKEN);        
+    }
+    
+    render() {
+        return (
+            <div id="map-container">
+                
+            </div>
+        )
+    }
 }
