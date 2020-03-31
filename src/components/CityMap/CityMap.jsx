458a3f401c418de48db13e59faee7a65547ca430
Daniel Escobar <daescobarp@unal.edu.co>
2020-03-31 14:55:10 -0500

    fixed modal always shown

--- src/components/CityMap/CityMap.jsx
+++ src/components/CityMap/CityMap.jsx
@@ -1,19 +1,14 @@
-import React from 'react'
-import mapboxgl from 'mapbox-gl'
+import React from "react";
 
 // import './CityMap.scss'
 
 export default class CityMap extends React.Component {
-    componentDidMount() {
-        const { REACT_APP_MAPBOX_ACCESS_TOKEN } = process.env
-        console.log(REACT_APP_MAPBOX_ACCESS_TOKEN);        
-    }
-    
-    render() {
-        return (
-            <div id="map-container">
-                
-            </div>
-        )
-    }
+  componentDidMount() {
+    const { REACT_APP_MAPBOX_ACCESS_TOKEN } = process.env;
+    console.log(REACT_APP_MAPBOX_ACCESS_TOKEN);
+  }
+
+  render() {
+    return <div id="map-container"></div>;
+  }
 }
